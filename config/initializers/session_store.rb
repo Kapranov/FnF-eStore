f43496aeafc584260fadddc2261d404cd77fba35
Kapranov <lugatex@yahoo.com>
2015-09-05 19:39:08 +0300

    added session db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
 Rails.application.config.session_store :cookie_store, key: '_fnf-estore_session'
+# Rails.application.config.session_store :active_record_store, key: '_fnf-estore_session'
+# FnfEstore::Application.config.session_store :active_record_store, :key => '_fnf-estore_session'
