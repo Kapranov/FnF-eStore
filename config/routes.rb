f05c2cec7feada266286a73adc42a723b90e27e5
Kapranov <lugatex@yahoo.com>
2015-09-05 11:12:29 +0300

    added registered controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
+  get 'visitors/index'
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
   resources :users
 end
