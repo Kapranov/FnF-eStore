ec172340f34d0d29c56f1b87e23b9c7e90de351e
Kapranov <lugatex@yahoo.com>
2015-09-07 14:53:12 +0300

    added en ru uk locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'settings/change_locale'
+
   mount Upmin::Engine => '/admin'
+
   get 'visitors/index'
+  get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
+
   root to: 'visitors#index'
+
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :users
 
   %w( 404 422 500 ).each do |code|
     get code, :to => "errors#show", :code => code
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@ puts 'CREATED ADMIN USER: ' << user.email
 <%= f.file_field :profile_image, id: "area-upload", class: "dropzone dz-clickable dz-default dz-message" do %>
   <span>Drop files here to upload</span>
 <% end %>
+
+https://github.com/fnando/i18n-js/issues/305
