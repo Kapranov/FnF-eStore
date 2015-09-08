427ab5a1854dc2744e9261fb773fee48769092a2
Kapranov <lugatex@yahoo.com>
2015-09-08 21:51:31 +0300

    added modules

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
-//= require jquery
+//= require jquery2
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require dropzone/dropzone.js
+//= require pace/pace.min.js
 //= require_tree .
 
 $(document).ready(function(){
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,11 @@
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( animate/animate.css)
 Rails.application.config.assets.precompile += %w( dropzone/basic.css dropzone/dropzone.css dropzone/dropzone.js )
+Rails.application.config.assets.precompile += %w( classie/classie.js )
+Rails.application.config.assets.precompile += %w( gritter/jquery.gritter.css gritter/jquery.gritter.min.js )
+Rails.application.config.assets.precompile += %w( iCheck/green.css iCheck/icheck.min.js )
+Rails.application.config.assets.precompile += %w( metisMenu/metisMenu.css metisMenu/metisMenu.js )
+Rails.application.config.assets.precompile += %w( pace/themes/green/pace-theme-barber-shop.css pace/pace.min.js )
+Rails.application.config.assets.precompile += %w( slimscroll/jquery.slimscroll.js )
+Rails.application.config.assets.precompile += %w( summernote/summernote.css summernote/summernote-bs3.css summernote/summernote.min.js summernote/summernote-ru-RU.js summernote/summernote-uk-UA.js )
+Rails.application.config.assets.precompile += %w( toastr/toastr.min.css toastr/toastr.min.js )
